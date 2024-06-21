5f76f18bb2bc71d451cede2658202e28b3dc5743
Rapid <134418161+Rapid-zets@users.noreply.github.com>
2024-06-21 09:25:00 -0400

    Update nodoSimple.java

--- nodoSimple.java
+++ nodoSimple.java
@@ -1,3 +1,25 @@
 public class nodoSimple {
-    
+    private libros libros;
+    private nodoSimple siguiente;
+
+    public nodoSimple(libros libros) {
+        this.libros = libros;
+        this.siguiente = null;
+    }
+
+    public libros getLibro() {
+        return libros;
+    }
+
+    public void setLibro(libros libros) {
+        this.libros = libros;
+    }
+
+    public nodoSimple getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(nodoSimple siguiente) {
+        this.siguiente = siguiente;
+    }
 }
